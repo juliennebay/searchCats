dffedb861b1fbb3a809713afed82f11adceef8a2
Julie <julienne.bay@gmail.com>
2019-09-04 18:11:15 -0400

    when the button is clicked, two things happen
    1. add a placeholder "li".
    2. delete any existing li from the previous click.

--- searchCats.js
+++ searchCats.js
@@ -1,11 +1,17 @@
 function loadScript(){
    const button = document.getElementsByTagName("button")[0];
    
-   function underConstruction(){
-       const message = document.getElementsByTagName("p")[0]
-       message.textContent = "site under construction. check back l8r"
+   function search(){
+       const ul = document.querySelector(".search-results")
+       // clear old results, if any
+       Array.from(ul.children).forEach(e => e.remove())
+       
+       // adding new search results
+       const li = document.createElement("li")
+       li.textContent = new Date()
+       ul.appendChild(li)  
    }
 
-   button.addEventListener("click", underConstruction)
+   button.addEventListener("click", search)
 }
 document.addEventListener("DOMContentLoaded", loadScript)
